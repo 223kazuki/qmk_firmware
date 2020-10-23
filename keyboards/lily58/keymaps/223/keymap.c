e3c1b584b4a18584ca6ca5f79de1bb1df7b64b11
Kazuki Tsutsumi <rixi223.kazuki@gmail.com>
2020-10-23 09:19:05 +0900

    Update

--- keyboards/lily58/keymaps/223/keymap.c
+++ keyboards/lily58/keymaps/223/keymap.c
@@ -28,6 +28,8 @@ extern uint8_t is_master;
 #define KC_MAPP LSFT(KC_F10)
 #define KC_ALFT LALT(KC_LEFT)
 #define KC_ARGT LALT(KC_RGHT)
+#define KC_CTAB LCTL(KC_TAB)
+#define KC_CSTB LCTL(LSFT(KC_TAB))
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -65,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  F12 |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  `   |  +   |  7   |  8   |  9   |  0   |                    | Tab  | ESC  |PageUp|PageDn|  =   |  -   |
+ * |  `   |  +   |  7   |  8   |  9   |  0   |                    | CSTab| ESC  |PageUp|PageDn| CTab |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | LGUI |  -   |  4   |  5   |  6   |  /   |-------.    ,-------| Home | Left |  Up  | Down |Right | End  |
  * |------+------+------+------+------+------|   (   |    |   )   |------+------+------+------+------+------|
@@ -77,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
   [_LOWER] = LAYOUT( \
   KC_F12,  KC_F1,   KC_F2,  KC_F3,  KC_F4,  KC_F5,                    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
-  KC_GRV,  KC_PPLS, KC_P7,  KC_P8,  KC_P9,  KC_P0,                    KC_TAB,  KC_ESC,  KC_PGUP, KC_PGDN, KC_EQL,  KC_MINS, \
+  KC_GRV,  KC_PPLS, KC_P7,  KC_P8,  KC_P9,  KC_P0,                    KC_CSTB, KC_ESC,  KC_PGUP, KC_PGDN, KC_CTAB, KC_MINS, \
   _______, KC_PMNS, KC_P4,  KC_P5,  KC_P6,  KC_PSLS,                  KC_HOME, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_END, \
   _______, KC_PAST, KC_P1,  KC_P2,  KC_P3,  KC_PEQL, KC_LPRN,KC_RPRN, KC_BSPC, KC_ALFT, KC_TILD, KC_EXLM, KC_ARGT, KC_AT, \
                             KC_P0,  KC_P0,  KC_PDOT, _______, KC_ENT, KC_HASH, KC_DLR,  KC_PERC \
